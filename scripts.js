f041f7b3ba234b167754effafcbfe98269b3a4b9
Jayant Babu <jayantbabu@MacBook-Pro.local>
2024-04-27 17:54:10 -0700

    Website v1

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Add interactive JavaScript here
-    console.log("Document ready!");
+    const starsContainer = document.getElementById('stars-container');
+    for (let i = 0; i < 200; i++) {
+        let star = document.createElement('div');
+        star.className = 'star';
+        star.style.top = `${Math.random() * window.innerHeight}px`;
+        star.style.left = `${Math.random() * window.innerWidth}px`;
+        starsContainer.appendChild(star);
+    }
 });
